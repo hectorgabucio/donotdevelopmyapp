1e6b0010f466504164f76abff023b6ab316d1932
hectorgabucio <hectorcompangabucio@gmail.com>
2020-09-20 18:34:07 +0200

    deployed redis master on k8s, connecting with client

--- internal/data/cache.go
+++ internal/data/cache.go
@@ -49,7 +49,7 @@ func initConnectionCache() CacheClient {
 	onceCache.Do(func() { // <-- atomic, does not allow repeating
 
 		conn := redis.NewClient(&redis.Options{
-			Addr:     "redis:6379",
+			Addr:     "redis-master.default.svc.cluster.local:6379",
 			Password: "", // no password set
 			DB:       0,  // use default DB
 		})
